545006763c8d7218998ed9189d22cd024d21359d
Sadik <sidwin20@gmail.com>
2025-05-18 23:56:30 +0300

    Update connection.js

--- backend/javascript_class/connection.js
+++ backend/javascript_class/connection.js
@@ -6,7 +6,7 @@ import mysql from "mysql2/promise";
 const connection = await mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "(Sadik3935)",
+  password: "your password",
   database: "zewditu_pms",
 });
 
